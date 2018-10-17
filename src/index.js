161c59604c1e7c1e3de85fd85be8d4dd97c24194
Federico Arambarri <far@clariusconsulting.net>
2018-10-17 20:30:03 -0300

    Starting point

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.listen(3000, function () {
+    console.log("server on port 3000");
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server on port 3000");
+});
