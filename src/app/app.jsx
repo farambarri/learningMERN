5f2732db2bef415906125a6b24c6b6227d64bb7b
Federico Arambarri <far@clariusconsulting.net>
2018-10-19 08:27:16 -0300

    react

---
+++ src/app/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+const { Component } = require("react");
+const { render } = require("react-dom");
+
+class App extends Component {
+  render() {
+    return <h1>Fred World</h1>;
+  }
+}
+
+export default App;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-const { Component } = require("react");
 const { render } = require("react-dom");
-
-class App extends Component {
-  render() {
-    return <h1>Hello World</h1>;
-  }
-}
-
-export default App;
+import App from "./app.jsx";
 
 render(<App />, document.getElementById("app"));
